9cf485e0aa8bf3a4c2ad1ccb83affad0629636f5
Robert Wikman <1263192+rbw@users.noreply.github.com>
2022-01-07 23:51:18 +0100

    Fix config

--- app/__main__.py
+++ app/__main__.py
@@ -5,12 +5,12 @@ from .controllers import AlbumController, TrackController
 from .server import Server
 
 # Set up config from environment
-listen_host, listen_port = env.get("RCRD_LISTEN", "127.0.0.1:5000").split(":")
-pg_username = env.get("AIOLI_PG_USERNAME")
-pg_password = env.get("AIOLI_PG_PASSWORD")
-pg_address = env.get("AIOLI_PG_ADDRESS")
-pg_database = env.get("AIOLI_PG_DATABASE")
-log_config = env.get("AIOLI_LOG_CONFIG")
+listen_host, listen_port = env.get("AIOLI_LISTEN", "127.0.0.1:5000").split(":")
+pg_username = env.get("AIOLI_PG_USERNAME", "songmeta")
+pg_password = env.get("AIOLI_PG_PASSWORD", "songmetasecret")
+pg_address = env.get("AIOLI_PG_ADDRESS", "localhost:5432")
+pg_database = env.get("AIOLI_PG_DATABASE", "songmeta")
+log_config = env.get("AIOLI_LOG_CONFIG", "logging.conf")
 log_debug = env.get("AIOLI_LOG_DEBUG", "1") == "1"
 
 
@@ -18,7 +18,7 @@ def main():
     return Server(
         db_url=f"postgresql+asyncpg://{pg_username}:{pg_password}@{pg_address}/{pg_database}",
         debug=log_debug,
-        controllers=[],
+        controllers=[AlbumController, TrackController],
     )
 
 
