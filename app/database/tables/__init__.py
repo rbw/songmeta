7b22d3782b4b988399e7a519a96c2046f0402f77
Robert Wikman <1263192+rbw@users.noreply.github.com>
2022-01-07 23:50:50 +0100

    Import models in database.tables

--- app/database/tables/__init__.py
+++ app/database/tables/__init__.py
@@ -1,1 +1,3 @@
-from .album import AlbumModel
+from .album import AlbumModel, AlbumTrackModel, AlbumStore
+from .artist import ArtistModel, ArtistRole
+from .track import TrackModel, TrackArtistModel, TrackVersion
